d5b6eeb57c7e8381db4d1e459751d0f75ad5d5b0
Chris Tweedie <cltweedie@gmail.com>
2020-11-25 12:14:51 +0000

    Actually fix it

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/homeflow/homeflowjs-docs/edit/master/docs/',
+            'https://github.com/homeflow/homeflowjs-docs/edit/master/',
         },
         blog: {
           showReadingTime: true,
